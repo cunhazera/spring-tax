5b1784546cbdf17ae115b9ab231db935fc404af2
cunhazera <cunha.gabriel919@gmail.com>
2016-09-30 21:22:52 -0300

    Test

--- src/test/java/com/money/TaxApplicationTests.java
+++ src/test/java/com/money/TaxApplicationTests.java
@@ -6,20 +6,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.money.application.TaxApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = TaxApplication.class)
+@SpringBootTest(classes = TaxApplication.class)
 public class TaxApplicationTests {
 
 	private MockMvc mockMvc;
@@ -27,12 +27,16 @@ public class TaxApplicationTests {
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
+	@Before
+	public void init() {
+		mockMvc = webAppContextSetup(webApplicationContext).build();
+	}
+
 	@Test
 	public void testStatusRequest() throws Exception {
-		mockMvc = webAppContextSetup(webApplicationContext).build();
-		MvcResult response = mockMvc.perform(get("/status")).andReturn();
-		assertThat(response.getResponse().getContentAsString(), equalTo("OK"));
-		assertThat(response.getResponse().getStatus(), is(200));
+		MockHttpServletResponse response = mockMvc.perform(get("/status")).andReturn().getResponse();
+		assertThat(response.getContentAsString(), equalTo("OK"));
+		assertThat(response.getStatus(), is(200));
 	}
 
 }
