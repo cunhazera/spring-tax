4f384c847e1de88186c08d5afd5932c830b00636
cunhazera <cunha.gabriel919@gmail.com>
2016-09-30 21:20:04 -0300

    Improving test

--- src/test/java/com/money/TaxApplicationTests.java
+++ src/test/java/com/money/TaxApplicationTests.java
@@ -1,28 +1,38 @@
 package com.money;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.money.application.TaxApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = TaxApplication.class)
 public class TaxApplicationTests {
-	
-	@Autowired
-    private TestRestTemplate restTemplate;
 
-    @Test
-    public void exampleTest() {
-        String body = this.restTemplate.getForObject("/status", String.class);
-        MatcherAssert.assertThat(body, CoreMatchers.equalTo("OK"));
-    }
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Test
+	public void testStatusRequest() throws Exception {
+		mockMvc = webAppContextSetup(webApplicationContext).build();
+		MvcResult response = mockMvc.perform(get("/status")).andReturn();
+		assertThat(response.getResponse().getContentAsString(), equalTo("OK"));
+		assertThat(response.getResponse().getStatus(), is(200));
+	}
 
 }
